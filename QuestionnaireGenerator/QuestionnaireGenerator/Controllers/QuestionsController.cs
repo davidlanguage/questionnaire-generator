7486cee14f5e31f4e8b2c8a4313fca38fac409d6
Javier Padin <javier.padin@solera.com>
2022-11-07 12:04:49 +0100

    controller work

--- QuestionnaireGenerator/QuestionnaireGenerator/Controllers/QuestionsController.cs
+++ QuestionnaireGenerator/QuestionnaireGenerator/Controllers/QuestionsController.cs
@@ -1,35 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuestionnaireGenerator.ViewModels;
+using System.Diagnostics;
 
 namespace QuestionnaireGenerator.Controllers
 {
     public class QuestionsController : Controller
     {
+
         [HttpGet]
         [Route("[controller]/{userId}")]
         public IActionResult GetQuestion()
         {
+
+            // TODO hardcoded viewmodel
             NextQuestion question = new NextQuestion();
             question.QuestionStatement = "¿Hola?";
             question.AnswerOptions = new List<string>();
             question.AnswerOptions.Add("A");
             question.AnswerOptions.Add("B");
             question.AnswerOptions.Add("C");
-            //display the question
-            //Question q = new Question();
+
             return View("NextQuestion", question);
         }
 
         [HttpPost]
-        [Route("[controller]/{userId}")]
-        public IActionResult SendAnswer()
+        [Route("[controller]/{userId}/q/{questionId}")]
+        public IActionResult SendAnswer(int userId, int questionId)
         {
-            // send the answer to the database
-            // display the next question
-            // if no more questions, display results
-            var req = Request.Body;
-            Console.WriteLine(req);
-            return View();
+            // save the answer to the database, then return the redirect
+
+            return RedirectToAction("GetQuestion", new { userId = userId, });
         }
 
 
